d3001925e3465d3fb38bd4d1b5dcce6758789814
Juan Ignacio <juan.lorca@uc.cl>
2025-04-23 17:00:40 -0400

    light card v1

--- src/main.py
+++ src/main.py
@@ -1,32 +1,71 @@
-import numpy as np
 import pickle
+import time
+import csv
+import numpy as np
+
+
+def write_data_file(data, csv_file, fieldnames): 
+    with open(csv_file, "a") as file: 
+        csv_writer = csv.DictWriter(file, fieldnames)
+        info = {
+            "pred_time": data[0],
+            "pred": data[1]
+        }
+        csv_writer.writerow(info)
 
 
 def LightCardLocal():
 
-    model_path = "models/model_F1.1__results.pkl_class1.pkl"                
-    with open(model_path, 'rb') as archivo:
-        current_model = pickle.load(archivo)
+    models = []
+    tests = []
+    model_paths = "models/model_F1.{}__results.pkl_class{}.pkl"
+    test_paths = "test_sets/F1.{}__results.pkl_class{}_test.npy"
 
-    archivo = np.load('test_sets/F1.1__results.pkl_class1_test.npy')
-    data = archivo[:, :-2]  
+    LC_times = []
+    LC_times_path = "/results/LC_times_E1.{}_C{}.csv"
 
-    print('cantidad de filas:', len(data))
+    for i in range(1, 4): 
+        for j in range(1, 4):
+            models.append(model_paths.format(i, j))
+            tests.append(test_paths.format(i, j))
+            LC_times.append(LC_times_path.format(i, j))
 
-    total_predictions = 0
-    for row in data:
-        row2 = row.reshape(1,-1)
-        prediction = current_model.predict(row2)
+    i = 0 
+    N = len(models)
+    fieldnames = ["pred_time", "pred"]
+    while i < N: 
 
-        print('Tamano de la predicción:', prediction.shape)
-        print('predicción:', prediction)
+        csv_file = LC_times[i]
+        with open(csv_file, "w") as file: 
+            csv_writer = csv.DictWriter(file, fieldnames)
+            csv_writer.writeheader()
 
-        total_predictions += 1
-        
-    print('predicciones hechas:', total_predictions)
+        model = models[i]
+        with open(model, 'rb') as archivo:
+            current_model = pickle.load(archivo)
+
+        test = tests[i]
+        data = np.load(test)
+        data = data[:, :-2]
+        print('Amount of rows:', len(data))
+
+        for row in data:
+            row = row.reshape(1,-1)
+
+            start_time = time.time()
+            prediction = current_model.predict(row)
+            end_time = time.time()
+
+            total_predictions += 1
+
+            data = [end_time - start_time, prediction[0]]
+            write_data_file(data, csv_file, fieldnames)
+
+        print('Number of predictions made:', total_predictions)
+
+        i += 1 
 
 
 if __name__ == "__main__":
     LightCardLocal()
-
 
